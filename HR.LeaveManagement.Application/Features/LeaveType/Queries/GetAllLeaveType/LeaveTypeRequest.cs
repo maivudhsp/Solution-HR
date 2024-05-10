bbe45588438dc29217e0c43987107a07fb699e58
Maivu <maivanphuongvu.dhsp@gmail.com>
2024-05-10 10:10:52 +0700

    add hr.leavemanagement.application

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveType/LeaveTypeRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveType
+{
+    public record LeaveTypeRequest : IRequest<List<LeaveTypeDto>>
+    {
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveType/LeaveTypeRequestHandle.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contacts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveType
+{
+    public class LeaveTypeRequestHandle : IRequestHandler<LeaveTypeRequest, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveType;
+        private readonly IMapper _mapper;
+
+        public LeaveTypeRequestHandle(ILeaveTypeRepository leaveType,
+            IMapper mapper)
+        {
+            _leaveType = leaveType;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(LeaveTypeRequest request, CancellationToken cancellationToken)
+        {
+            //read from db
+            var leavetypes = await _leaveType.GetAsync();
+            //map to dto
+            var data = _mapper.Map<List<LeaveTypeDto>>(leavetypes);
+            //return data
+            return data;
+        }
+    }
+}
